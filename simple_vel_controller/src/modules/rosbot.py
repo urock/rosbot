c6755c39ffe8736caa46a29054c06eaf7fd94351
Yury Rumyantsev <urock@fastsense.tech>
2020-11-25 22:13:09 +0300

    code refactored added Robot class in module

---
+++ simple_vel_controller/src/modules/rosbot.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# license removed for brevity
+
+import math
+import numpy as np
+import rospy
+
+class RobotState():
+
+    def __init__(self, x = 0.0, y = 0.0, yaw = 0.0, vx = 0.0, vy = 0.0, w = 0.0):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.vx = vx
+        self.vy = vy
+        self.w = w
+
+
+class Goal():
+
+    def __init__(self, x = 0.0, y = 0.0):
+        self.x = x
+        self.y = y
+
+class Params():
+
+    def __init__(self):
+        self.v_max = 0.4
+        self.w_max = 0.4
+        self.xy_margin_squared = 0.2
+
+
+
+class Rosbot():
+
+
+    def __init__(self):
+        self.state = RobotState()
+        self.params = Params()
+
+
+    def set_odom_state(self, odom_state):
+        self.state = odom_state
+
+
+    def dist_to_goal_L2(self, goal):             
+        """
+        param goal - Goal Class object
+        """
+        return  (goal.x - self.state.x)**2 + (goal.y - self.state.y)**2 
+            
+
+    def goal_reached(self, goal):
+        # rospy.logwarn("checking goal: x -> {:.2f}, y -> {:.2f}".format(goal.x, goal.y))
+        return self.dist_to_goal_L2(goal) <= self.params.xy_margin_squared
+
+    def calculate_contol(self, goal):
+        #rospy.logwarn("calc control: state -> ({:.2f}, {:.2f}) goal -> ({:.2f}, {:.2f})".
+                        format(self.state.x, self.state.y, goal.x, goal.y))
+        rho = self.dist_to_goal_L2(goal)
+
+        
+        azim_goal = math.atan2((goal.y - self.state.y),(goal.x - self.state.x))
+        alpha = azim_goal - self.state.yaw                                        
+
+        if (abs(alpha) > math.pi): 
+            alpha -= np.sign(alpha) * 2 * math.pi
+
+        v = self.params.v_max * math.tanh(rho) * math.cos(alpha)
+        w = self.params.w_max * alpha + math.tanh(rho)*math.sin(alpha)*math.cos(alpha)/rho
+
+        return v, w
+
+
--- simple_vel_controller/src/simple_vel.py
+++ simple_vel_controller/src/simple_vel.py
@@ -16,30 +16,32 @@ from tf.transformations import quaternion_from_euler
 
 from nav_msgs.msg import Path
 
+from modules.rosbot import Rosbot, RobotState, Goal
+
 # from visualization_msgs.msg import Marker
 
 
-class Bot():
-
+class TrajKeeper():
+    """
+    Gived Robot and Controll module this modules tries to follow given path 
+    Trajectory should be defined as array of (x, y, yaw ?) points
+    """
     
 
     def __init__(self, node_name): 
         self.node_name = node_name
         rospy.init_node(self.node_name, anonymous=True)
 
-        rospy.loginfo("Bot init")
+        rospy.loginfo("trajectory_keeper")
 
-        self.v_max = 0.4
-        self.w_max = 0.4
-
-        self.xy_margin_squared = 0.2
+        self.robot = Rosbot()
 
         self.tf_listener = tf.TransformListener()
 
         self.cmd_freq = 10 # Hz        
 
-        self.state = (0, 0, 0, 0, 0, 0)     # x, y, yaw, vx, vy,  w
-        self.goal = (0, 0, 0)               # x, y, yaw 
+        self.odom_state = RobotState()     # x, y, yaw, vx, vy,  w
+        self.current_goal = Goal()               # x, y 
         self.control = (0, 0)               # v, w
 
         self.goal_queue = []
@@ -51,36 +53,41 @@ class Bot():
         self.rate = rospy.Rate(self.cmd_freq)
 
 
-    def get_current_state(self):
+    def set_robot_odom_state(self):
         src_frame = 'odom'
         dst_frame = 'base_link'
         try:
             (coord,orient) = self.tf_listener.lookupTransform(src_frame, dst_frame, rospy.Time())
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             rospy.logerr("Error in lookup transform from {} to {}".format(src_frame, dst_frame))    
-            return
+            return False
 
         x, y = coord[0], coord[1]
         yaw = tf.transformations.euler_from_quaternion(orient)[2]
 
-        vx = (x   - self.state[0]) * self.cmd_freq
-        vy = (y   - self.state[1]) * self.cmd_freq
-        w  = (yaw - self.state[2]) * self.cmd_freq
+        vx = (x   - self.odom_state.x) * self.cmd_freq
+        vy = (y   - self.odom_state.y) * self.cmd_freq
+        w  = (yaw - self.odom_state.yaw) * self.cmd_freq
 
-        self.state = (x, y, yaw, vx, vy, w)
+        self.odom_state = RobotState(x, y, yaw, vx, vy, w)
 
-    def print_state(self):
+        self.robot.set_odom_state(self.odom_state)
+
+        return True
+
+    def print_state(self, st):
         rospy.loginfo(" x -> {:.2f}, y -> {:.2f}, yaw -> {:.2f}, vx -> {:.2f}, vy -> {:.2f}, w -> {:.2f}".format(
-                        self.state[0], self.state[1], self.state[2], self.state[3], self.state[4], self.state[5]))
+                        st.x, st.y, st.yaw, st.vx, st.vy, st.w))
 
     def goal_callback(self, msg):
         x, y = msg.pose.position.x, msg.pose.position.y            
-        self.goal_queue.append((x, y))
+        self.goal_queue.append(Goal(x, y))
         rospy.logwarn("added goal to queue: x -> {:.2f}, y -> {:.2f}".format(x, y))
         
     def path_callback(self, msg):
         for p in msg.poses:
-            self.goal_queue.append((p.pose.position.x, p.pose.position.y))
+            x, y = p.pose.position.x, p.pose.position.y
+            self.goal_queue.append(Goal(x, y))
     
     def publish_twist(self, v, w):
         """
@@ -96,55 +103,30 @@ class Bot():
         twist_cmd.angular.z = w
         self.cmd_pub.publish(twist_cmd)
 
-    # (x_goal - x_cur)^2 + (y_goal - y_cur)^2
-    def dist_to_goal(self):
-        return  (self.goal[0] - self.state[0])**2 + (self.goal[1] - self.state[1])**2 
-
-    def get_current_goal(self):
-        rho = self.dist_to_goal()
-        
-        if rho < self.xy_margin_squared:
-            if self.goal_queue:
-                self.goal = self.goal_queue.pop(0)
-                rospy.logerr("new current_goal: x -> {:.2f}, y -> {:.2f}".format(self.goal[0], self.goal[1]))
-
-
-    def calc_control(self):
-        rho = self.dist_to_goal()
-
-        if rho < self.xy_margin_squared:
-            if not self.goal_queue:
-                self.control = (0.0, 0.0)
-                return
-
-        azim_goal = math.atan2((self.goal[1] - self.state[1]),(self.goal[0] - self.state[0]))
-        alpha = azim_goal - self.state[2]                                        
-
-        if (abs(alpha) > math.pi): 
-            alpha -= np.sign(alpha) * 2 * math.pi
-
-        v = self.v_max * math.tanh(rho) * math.cos(alpha)
-        w = self.w_max * alpha + math.tanh(rho)*math.sin(alpha)*math.cos(alpha)/rho
-
-        self.control = (v, w)
-
-    
+      
 
     def run(self):
         while not rospy.is_shutdown():
-            self.get_current_state()
-            self.get_current_goal()
-            self.calc_control()
-            self.publish_twist(self.control[0], self.control[1])
-            # self.print_state()
+            if not self.set_robot_odom_state():
+                continue
+            # check if new goal should be selected and calc control
+            if self.robot.goal_reached(self.current_goal):
+                if self.goal_queue:
+                    self.current_goal = self.goal_queue.pop(0)
+                    rospy.logerr("new current_goal: x -> {:.2f}, y -> {:.2f}".
+                    format(self.current_goal.x, self.current_goal.y))
+                else:
+                    # end of trajectory
+                    self.publish_twist(0, 0)    
+                    continue
+
+            v, w = self.robot.calculate_contol(self.current_goal)
+            self.publish_twist(v, w)
             self.rate.sleep()
 
-
-
 def main():
-    bot = Bot('simple_vel')
-    bot.run()
-    rospy.loginfo("Bot End")
+    trajectory_keeper = TrajKeeper('trajectory_keeper')
+    trajectory_keeper.run()
 
 if __name__ == '__main__':
     main()
