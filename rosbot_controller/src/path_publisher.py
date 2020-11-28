f8412caf1f050b94ca59541e73fa191faf3cbc0a
Yury Rumyantsev <urock@fastsense.tech>
2020-11-28 22:43:01 +0300

    sin path generator implemented as function

--- rosbot_controller/src/path_publisher.py
+++ rosbot_controller/src/path_publisher.py
@@ -11,32 +11,14 @@ from nav_msgs.msg import Path
 from std_msgs.msg import Header
 from geometry_msgs.msg import PoseStamped
 
+def IsValidTrajType(traj_type):
+    return traj_type in ('sin', 'polygon')
 
-def main():
-    rospy.init_node("path_pub", anonymous=True)
-    rospy.loginfo("path_pub init")
-
-    path_topic_name = "/path"
-
-    path_pub = rospy.Publisher(path_topic_name, Path, queue_size=5)    
-
-    msg = Path()
-    msg.header.frame_id = "odom"
-    msg.header.stamp = rospy.Time.now()
-    cnt = 0
-    msg.header.seq = cnt
-    cnt += 1
-    msg.poses = []
-
-    x_ar = np.arange(0,2*np.pi,0.1)   # start,stop,step
+def SinTrajGenerator(msg, step):
+    x_ar = np.arange(0,2*np.pi, step)   # start,stop,step
     y_ar = np.sin(x_ar)
 
-    # path = [(x_ar[i], y_ar[i]) for ]
-    # path = [(2.0, 0), (2.0, 2.0), (0, 2.0), (0, 0)]
-
-    # plt.plot(x,y)
-    # plt.show()
-
+    cnt = 0
     for i in range(len(x_ar)):
         ps = PoseStamped()
         ps.header = msg.header
@@ -45,7 +27,34 @@ def main():
         ps.pose.position.x = x_ar[i]
         ps.pose.position.y = y_ar[i]
         ps.pose.position.z = 0 
-        msg.poses.append(ps)        
+        msg.poses.append(ps)  
+
+    return msg  
+
+
+def main():
+    rospy.init_node("path_pub", anonymous=True)
+    rospy.loginfo("path_pub init")
+    traj_type = rospy.get_param('~traj_type')
+
+    if not IsValidTrajType(traj_type):
+        rospy.logerr("Not valid traj type")
+        return
+
+    path_topic_name = "/path"
+
+    path_pub = rospy.Publisher(path_topic_name, Path, queue_size=5)    
+
+    msg = Path()
+    msg.header.frame_id = "odom"
+    msg.header.stamp = rospy.Time.now()
+    msg.header.seq = 0
+    msg.poses = []
+
+    step = 0.1
+
+    if traj_type == 'sin':
+        msg = SinTrajGenerator(msg, step)    
 
     while not rospy.is_shutdown():
         if path_pub.get_num_connections() > 1:
