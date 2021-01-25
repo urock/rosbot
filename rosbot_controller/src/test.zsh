0131093b78574d2559ef4add9aff94cade3d82c8
KostyaYamshanov <k.yamshanov@fastsense.tech>
2021-01-25 18:23:58 +0700

    update script

--- rosbot_controller/src/test.zsh
+++ rosbot_controller/src/test.zsh
@@ -24,16 +24,21 @@ done
 
 echo source $path_/devel/setup.zsh
 source $path_/devel/setup.zsh
+
 roslaunch rosbot2 urock_system.launch rviz:=true gui:=false &
-for traj in 2.5sin -1sin 3sin1.5 sin0.5 polygon
+# for traj in 2.5sin -1sin 3sin1.5 sin0.5 polygon
+for traj in 2.5sin polygon
   do
-    for lin_vel in 0.5 2.5 5.0 7.0 10.0
+    # for lin_vel in 0.5 2.5 5.0 7.0 10.0
+    for lin_vel in 4.0
       do
-        for cir_vel in 0.5 1.5 2.5 3.5 4.0
+        # for cir_vel in 0.5 1.5 2.5 3.5 4.0 
+        for cir_vel in 1.5 
           do
             rosparam set /max_lin_vel $lin_vel
             rosparam set /max_cir_vel $cir_vel
-            roslaunch plotter plotter.launch output_folder:=/traj=$traj-lin_vel=$lin_vel-cir_vel=$cir_vel timeout:=300 &
+            rosparam set /cmd_freq 10
+            roslaunch plotter plotter.launch output_folder:=/traj=$traj-lin_vel=$lin_vel-cir_vel=$cir_vel-cmd_freq=10 timeout:=300 &
             roslaunch rosbot2 test_rosbot.launch traj_type:=$traj
             ResetPose
           done      
@@ -59,20 +64,21 @@ rosnode kill --all
 
 
                        ### DONE ###
-#    traj         lin_vel               cir_vel    
-#   2.5sin    0.5 2.5 5.0 7 10    0.5 1.5 2.5 3.5 4.0  
+#    TRAJ         lin_vel               cir_vel  
+######################################################  
+#   2.5sin    0.5 2.5 5.0 7 10    0.5 1.5 2.5 3.5 4.0 
+#----------------------------------------------------- 
 #   -1sin     0.5 2.5 5.0 7 10    0.5 1.5 2.5 3.5 4.0
+#-----------------------------------------------------
+#             0.5 2.5 5.0         0.5 1.5 2.5 3.5 4.0 
+#   3sin1.5   7.0                 0.5 1.5
+#             10.0                0.5
+#----------------------------------------------------
+#   sin0.5    0.5 2.5 5.0 7 10    0.5 1.5 2.5 3.5 4.0
+#----------------------------------------------------
 #   polygon   0.5 2.5 5.0 7 10    0.5 1.5 2.5 3.5 4.0     
-#   3sin1.5   0.5 2.5 5.0         0.5 1.5 2.5 3.5 4.0 
-#   5sin0.5   0.5 2.5 5.0 7       0.5 1.5 2.5 3.5 4.0
-#   5sin0.5   10                  0.5 1.5 2.5
+
+
 #   triangle  0.5 2.5 5.0 7       0.5 1.5 2.5 3.5 4.0
 #   triangle  8.0                 0.5 1.5 
 #   triangle  10.0                1.5 
-
-
-                       ### FAIL ###
-#    traj         lin_vel               cir_vel     
-#   3sin1.5        7.0                2.5 3.5 4.0
-#   3sin1.5        10.0               0.5 1.5 2.5 3.5 4.0 
-#   5sin0.5        10.0               3.5 4.0
