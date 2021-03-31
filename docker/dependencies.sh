f6623988161753356996bc45eda06610d17e37a7
Alex <speedy_sky@mail.ru>
2021-03-31 18:40:37 +0300

    fix order of reloading docker daemon

--- docker/dependencies.sh
+++ docker/dependencies.sh
@@ -6,5 +6,6 @@ sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update
 
 sudo apt-get install -y nvidia-docker2
-sudo systemctl restart docker
 sudo apt-get install -y nvidia-container-toolkit
+
+sudo systemctl restart docker
