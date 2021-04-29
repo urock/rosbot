fix range lookahead, reworkred by Ruslan triangle_loss

rosbot_controller/src/mppi/modules/goal_handler.py
@@ -9,7 +9,7 @@ class GoalHandler:
         self.goal_idx = 0
 
         self._reference_trajectory: np.array
-        self._trajectory_lookahead = rospy.get_param("~goal_handler/traj_lookahead", 7)
+        self._trajectory_lookahed_count = rospy.get_param("~goal_handler/trajectory_lookahed_count", 7)
         self._goal_tolerance = rospy.get_param("~goal_handler/goal_tolerance", 0.2)
 
 
@@ -49,7 +49,7 @@ class GoalHandler:
 
     def _get_last_considered_trajectory_idx(self):
         traj_end = len(self._reference_trajectory)
-        end = self.goal_idx + self._trajectory_lookahead + 1
+        end = self.goal_idx + self._trajectory_lookahed_count + 1
         return min(end, traj_end)
 
     def _is_goal_reached(self, dist):

rosbot_controller/src/mppi/optimizers/mppic_generator.py
@@ -32,7 +32,6 @@ class MPPICGenerator():
         self._batch_of_seqs = np.zeros(shape=(self.batch_size, self.time_steps, 5))
         self._batch_of_seqs[:, :, 4] = self.dt
 
-    
     @property
     def velocities_batch(self):
         return self._batch_of_seqs[:, :, :2]
@@ -53,7 +52,6 @@ class MPPICGenerator():
         w_best = self.curr_control_seq[0 + offset, 1]
         return Control(v_best, w_best)
 
-
     def displace_controls(self, offset: int):
         if offset == 0:
             return
@@ -63,10 +61,14 @@ class MPPICGenerator():
         self.curr_control_seq = np.concatenate([control_cropped, end_part], axis=0)
 
     def _update_batch_of_seqs(self):
-        noises = self._generate_noises()
+        noises = self._generate_noises(int(self.time_steps / 2))
+        noises = np.repeat(noises, 2, axis=1)
+
         self._batch_of_seqs[:, 0, 0] = self.state.v
         self._batch_of_seqs[:, 0, 1] = self.state.w
+
         self._batch_of_seqs[:, :, 2:4] = self.curr_control_seq[None] + noises
+
         self._batch_of_seqs[:, :, 2:3] = np.clip(
             self._batch_of_seqs[:, :, 2:3], -self._limit_v, self._limit_v
         )
@@ -75,9 +77,9 @@ class MPPICGenerator():
         )
         self._update_velocities()
 
-    def _generate_noises(self):
-        v_noises = np.random.normal(0.0, self._v_std, size=(self.batch_size, self.time_steps, 1))
-        w_noises = np.random.normal(0.0, self._w_std, size=(self.batch_size, self.time_steps, 1))
+    def _generate_noises(self, time_steps):
+        v_noises = np.random.normal(0.0, self._v_std, size=(self.batch_size, time_steps, 1))
+        w_noises = np.random.normal(0.0, self._w_std, size=(self.batch_size, time_steps, 1))
         noises = np.concatenate([v_noises, w_noises], axis=2)
 
         return noises

rosbot_controller/src/mppi/optimizers/mppic_optimizer.py
@@ -15,6 +15,8 @@ class MPPICOptimizer():
         self.next_control_policy = next_control_policy
 
         self.reference_trajectory: np.ndarray
+        self.reference_intervals: np.array
+
         self.curr_trajectories: np.ndarray
         self.curr_offset = 0
 
@@ -22,7 +24,6 @@ class MPPICOptimizer():
         self._desired_v = rospy.get_param("~mppic/desired_v", 0.5)
 
         self._trajectory_lookahead = int(rospy.get_param("~mppic/traj_lookahead", 7))
-        self._goals_interval = rospy.get_param("~mppic/goals_interval", 0.1)
 
     def calc_next_control(self, goal_idx):
         start = perf_counter()
@@ -30,7 +31,8 @@ class MPPICOptimizer():
             self._optimize(goal_idx)
 
         self.curr_exec_time = perf_counter() - start
-        self.curr_offset = math.ceil(self.curr_exec_time / self.control_generator.dt)
+        self.curr_offset = min(math.ceil(self.curr_exec_time / self.control_generator.dt), 
+                len(self.control_generator.curr_control_seq) - 1)
 
         control = self.control_generator.get_control(self.curr_offset)
         self.control_generator.displace_controls(self.curr_offset)
@@ -49,21 +51,35 @@ class MPPICOptimizer():
     def _optimize(self, goal_idx: int):
         self.curr_trajectories = self.control_generator.generate_trajectories()
 
-        beg = goal_idx - 1 if goal_idx != 0 else 0
-        end = self._get_last_considered_trajectory_idx(goal_idx)
+        count = self._calc_considered_point_count(goal_idx)
+
+        beg = max(0, goal_idx - count)
+        end = min(goal_idx + count, len(self.reference_trajectory) - 1) + 1
+
         reference_considered = self.reference_trajectory[beg:end, :3]
+        intervals_considered = self.reference_intervals[beg:end - 1]
 
         costs = self.cost(
             self.curr_trajectories,
             reference_considered,
-            self._desired_v,
-            self._goals_interval,
+            intervals_considered,
+            self._desired_v
         )
 
         next_control_seq = self.next_control_policy(costs, self.control_generator.controls_batch)
         self.control_generator.curr_control_seq = next_control_seq
+        # print(self.control_generator.curr_control_seq.shape)
 
-    def _get_last_considered_trajectory_idx(self, goal_idx):
-        traj_end = len(self.reference_trajectory)
-        end = goal_idx + self._trajectory_lookahead + 1
-        return min(end, traj_end)
+    def _calc_considered_point_count(self, goal_idx):
+        count = 0
+        dist = 0
+        for q in range(goal_idx, len(self.reference_intervals)):
+            dist += self.reference_intervals[q]
+            count += 1
+            if dist > self._trajectory_lookahead:
+                break
+
+        return count
+
+
+

rosbot_controller/src/mppi/policies/costs.py
@@ -1,93 +1,124 @@
-from numba import jit
-from numba.experimental import jitclass
 import numpy as np
+from numba import njit
 
 
-from abc import ABC, abstractmethod
+def nearest_cost(state, reference_trajectory, reference_intervals, desired_v):
+    """Cost according to k nearest points.
+
+    Args:
+        state: np.ndarray of shape [batch_size, time_steps, 8] where 3 for x, y, yaw, v, w, v_control, w_control, dts
+        ref_traj: np.array of shape [ref_traj_size, 3] where 3 for x, y, yaw
+        traj_lookahead: int
+        goal_idx: int
+        goals_interval: float
+        desired_v: float
+
+    Return:
+        costs: np.array of shape [batch_size]
+    """
+    v = state[:, :, 3]
+
+    costs = lin_vel_cost(v, desired_v)
+    costs += nearest_cost_for_k_ellements(state, reference_trajectory, 3)
+    print(costs.shape)
+
+    return costs
 
 
-class Cost(ABC):
-    @abstractmethod
-    def __call__(self, state, reference_trajectory, desired_v, goals_interval):
-        """Calculate cost 
+def nearest_cost_for_k_ellements(state, reference_trajectory, k_idx):
+    x_dists = state[:, :, :1] - reference_trajectory[:, 0]
+    y_dists = state[:, :, 1:2] - reference_trajectory[:, 1]
+    dists = x_dists ** 2 + y_dists ** 2
 
-        Args:
-            state: np.ndarray of shape [batch_size, time_steps, 7] where 3 for x, y, yaw, v, w, v_control, w_control
-            ref_traj: np.array of shape [ref_traj_size, 3] where 3 for x, y, yaw
-            traj_lookahead: int
-            goal_idx: int
-            goals_interval: float
-            desired_v: float
+    k = min(k_idx, len(reference_trajectory))
+    dists = np.partition(dists, k - 1, axis=2)[:, :, :k] * np.arange(1, k + 1)
 
-        Return:
-            costs: np.array of shape [batch_size]
-        """
-class TriangleCost():
-    def __call__(self, state, reference_trajectory, desired_v, goals_interval):
-        """Cost according to nearest segment.
+    return dists.min(2).sum(1)
 
-        Args:
-            state: np.ndarray of shape [batch_size, time_steps, 8] where 3 for x, y, yaw, v, w, v_control, w_control, dts
-            ref_traj: np.array of shape [ref_traj_size, 3] where 3 for x, y, yaw
-            traj_lookahead: int
-            goal_idx: int
-            goals_interval: float
-            desired_v: float
+@njit
+def triangle_cost(state, reference_trajectory, reference_intervals, desired_v):
+    """Cost according to nearest segment.
 
-        Return:
-            costs: np.array of shape [batch_size]
-        """
-        v = state[:, :, 3]
-        w = state[:, :, 5]
+    Args:
+        state: np.ndarray of shape [batch_size, time_steps, 8] where 3 for x, y, yaw, v, w, v_control, w_control, dts
+        ref_traj: np.array of shape [ref_traj_size, 3] where 3 for x, y, yaw
+        desired_v: float
 
-        v_control = state[:, :, 5]
-        w_control = state[:, :, 6]
+    Return:
+        costs: np.array of shape [batch_size]
+    """
+    v = state[:, :, 3]
+    costs = lin_vel_cost(v, desired_v)
+    costs = costs + triangle_cost_segments(state, reference_trajectory, reference_intervals)
 
-        costs = lin_vel_cost(v, desired_v)
-        costs += self._triangle_cost_segments(state, reference_trajectory, goals_interval)
-
-        return costs
-
-    def _triangle_cost_segments(self, state, ref, goals_interval):
-        x_dists = state[:, :, :1] - ref[:, 0]
-        y_dists = state[:, :, 1:2] - ref[:, 1]
-        dists = np.sqrt(x_dists ** 2 + y_dists ** 2)
-        triangle_costs = np.empty(shape=(dists.shape[0], dists.shape[1], len(ref) - 1))
-
-        if len(ref) == 1:
-            return dists.squeeze(2).sum(1)
-
-        for q in range(len(ref) - 1):
-            first_sides = dists[:, :, q]
-            second_sides = dists[:, :, q + 1]
-            opposite_sides = goals_interval
-            triangle_costs[:, :, q] = self._triangle_cost_segment(
-                opposite_sides, first_sides, second_sides
-            )
-
-        return triangle_costs.min(2).sum(1)
-
-    def _triangle_cost_segment(self, opposite_side, first_sides, second_sides):
-        costs = np.empty(shape=first_sides.shape)
-        first_obtuse_mask = self._is_angle_obtuse(first_sides, second_sides, opposite_side)
-        second_obtuse_mask = self._is_angle_obtuse(second_sides, first_sides, opposite_side)
-        h_mask = (~first_obtuse_mask) & (~second_obtuse_mask)
-
-        costs[first_obtuse_mask] = second_sides[first_obtuse_mask]
-        costs[second_obtuse_mask] = first_sides[second_obtuse_mask]
-        costs[h_mask] = self._heron(opposite_side, first_sides[h_mask], second_sides[h_mask])
-
-        return costs
-
-    def _is_angle_obtuse(self, opposite_side, b, c):
-        return opposite_side ** 2 > (b ** 2 + c ** 2)
-
-    def _heron(self, opposite_side, b, c):
-        p = (opposite_side + b + c) / 2.0
-        h = 2.0 / opposite_side * np.sqrt(p * (p - opposite_side) * (p - b) * (p - c))
-        return h
+    return costs
 
 
+@njit
+def triangle_cost_segments(state, reference_trajectory, reference_intervals):
+    x_dists = state[:, :, :1] - reference_trajectory[:, 0]
+    y_dists = state[:, :, 1:2] - reference_trajectory[:, 1]
+    dists = np.sqrt(x_dists ** 2 + y_dists ** 2)
+    
+    if len(reference_trajectory) == 1:
+        return dists.sum(2).sum(1)
+
+    first_sides = dists[:, :, :-1]
+    second_sides = dists[:, :, 1:]
+    opposite_sides = reference_intervals
+
+
+    first_obtuse_mask = is_angle_obtuse(first_sides, second_sides, opposite_sides)
+    second_obtuse_mask = is_angle_obtuse(second_sides, first_sides, opposite_sides)
+
+    cost = np.zeros(shape=(dists.shape[0]))
+    for i in range(dists.shape[0]):
+        for j in range(dists.shape[1]):
+            dists_to_segments = np.empty(len(reference_intervals))
+            for k in range(len(reference_intervals)):
+                first_side = first_sides[i, j, k]
+                second_side = second_sides[i, j, k]
+                opposite_side = opposite_sides[k]
+                if is_angle_obtuse(first_side, second_side, opposite_side):
+                    dists_to_segments[k] = first_side
+                elif is_angle_obtuse(second_side, first_side, opposite_side):
+                    dists_to_segments[k] = second_side
+                else:
+                    dists_to_segments[k] = heron(
+                        opposite_side,
+                        first_side,
+                        second_side
+                    )
+            cost[i] += dists_to_segments.min()
+
+    return cost
+
+@njit
+def min_costs_2dim(costs):
+    min_costs = np.empty(shape = (costs.shape[0], costs.shape[1]))
+    
+    for q in range(costs.shape[0]):
+        for w in range(costs.shape[1]):
+            min_costs[q][w]= costs[q][w].min()
+
+    return min_costs
+
+
+
+@njit
+def is_angle_obtuse(opposite_side, b, c):
+    return opposite_side ** 2 > (b ** 2 + c ** 2)
+
+
+
+@njit
+def heron(opposite_side, b, c):
+    p = (opposite_side + b + c) / 2.0
+    h = 2.0 / opposite_side * np.sqrt(p * (p - opposite_side) * (p - b) * (p - c))
+    return h
+
+
+@njit
 def lin_vel_cost(v, desired_v):
     DESIRED_V_WEIGHT = 3.0
     v_costs = DESIRED_V_WEIGHT * ((v - desired_v)**2).sum(1)