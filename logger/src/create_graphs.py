81af40c5fffc5eb996a2484bf5b03530ca68677d
KostyaYamshanov <k.yamshanov@fastsense.tech>
2021-07-05 18:13:50 +0700

    fix NN model logging

--- logger/src/create_graphs.py
+++ logger/src/create_graphs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
-import seaborn as sns
+# import seaborn as sns
 import pandas as pd
 import numpy as np
 import argparse
@@ -76,17 +76,18 @@ def plot_for_one_trajectory(args, folder_path):
     ax[1].set_xlabel('t, sec')
     ax[0].set_title("linear velocity and control")
     plot_xy_data(x=time['t'], y=robot_state['v'], ax=ax[0], plot_name="robot v")
-    plot_xy_data(x=time['t'], y=model_state['v'], ax=ax[0], plot_name="kinematic model v")
     plot_xy_data(x=time['t'], y=control['x'], ax=ax[0], plot_name="u1")
-    if nn_model_state is not None:
-        # print(nn_model_state['v'])
+    if len(model_state['v']) > 0:
+        plot_xy_data(x=time['t'], y=model_state['v'], ax=ax[0], plot_name="kinematic model v")
+    if nn_model_state is not None or len(nn_model_state['v']) >0:
         plot_xy_data(x=time['t'], y=nn_model_state['v'], ax=ax[0], plot_name="nn model v")
 
     ax[1].set_title("angular velocity and control")
     plot_xy_data(x=time['t'], y=robot_state['w'], ax=ax[1], plot_name="robot w")
-    plot_xy_data(x=time['t'], y=model_state['w'], ax=ax[1], plot_name="kinematic model w")
     plot_xy_data(x=time['t'], y=control['yaw'], ax=ax[1], plot_name="u2")
-    if nn_model_state is not None:
+    if len(model_state['w']) > 0:
+        plot_xy_data(x=time['t'], y=model_state['w'], ax=ax[1], plot_name="kinematic model w")
+    if nn_model_state is not None or len(nn_model_state['w']) > 0:
         plot_xy_data(x=time['t'], y=nn_model_state['w'], ax=ax[1], plot_name="nn model w")
 
 
@@ -131,10 +132,10 @@ def plot_for_one_trajectory(args, folder_path):
     ax4.set_xlabel('X, m')        
     ax4.set_ylabel('Y, m')
     ax4.set_title("XY trajectory")
-    plot_xy_data(x=robot_state['x'], y=robot_state['y'], ax=ax4, plot_name="x_y")
-    plot_xy_data(x=model_state['x'], y=model_state['y'], ax=ax4, plot_name="kinematic model x_y")
+    plot_xy_data(x=robot_state['x'], y=robot_state['y'], ax=ax4, plot_name="ground truth")
+    plot_xy_data(x=model_state['x'], y=model_state['y'], ax=ax4, plot_name="kinematic model")
     if nn_model_state is not None:
-        plot_xy_data(x=nn_model_state['x'], y=nn_model_state['y'], ax=ax4, plot_name="nn model x_y")
+        plot_xy_data(x=nn_model_state['x'], y=nn_model_state['y'], ax=ax4, plot_name="NN model")
     
     if args.output_folder != "":
         save_plot(path=args.output_folder, name="Y over X")        
